8aa3903f0ec360ebc5627a268d32f90dbf8717f6
BogdanZavadka <bogdanzavadka@gmail.com>
2022-04-08 21:31:31 +0300

    lab2

---
+++ lab21/src/iot/PetShop/Main.java
@@ -0,0 +1,15 @@
+package iot.PetShop;
+
+import iot.PetShop.manager.AnimalManager;
+import iot.PetShop.manager.PetSeller;
+import iot.PetShop.models.AnimalInfo;
+import iot.PetShop.models.AnimalSize;
+
+public class Main {
+    public static void main(String[] args) {
+        AnimalManager animalManager = new AnimalManager();
+        AnimalInfo animalInfo = new AnimalInfo();
+        AnimalSize animalSize = new AnimalSize();
+        PetSeller petSeller = new PetSeller();
+    }
+}
